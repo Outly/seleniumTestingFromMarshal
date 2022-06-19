722f5c64a12547961a098e9688f582495c7cd63d
Outly <kamenschool41@yandex.ru>
2022-06-19 16:32:08 +0600

    add findElement and click

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
 
-        /* Установка драйвера, открытие сайта с помощью драйвера */
+        // Установка драйвера, открытие сайта с помощью драйвера
 
         System.setProperty("webdriver.chrome.driver","D:\\webdrivers\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
+
+        // Создаём элемент и ссылаемся на него по значению его атрибута
+        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+
+        // Кликаем на элемент
+        input.click();
     }
 }
