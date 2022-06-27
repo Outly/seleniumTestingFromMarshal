4f8674eba283fcf93b5e36e9ca34a522e99e4bd2
Outly <kamenschool41@yandex.ru>
2022-06-27 16:36:24 +0600

    add provider testng

--- src/main/java/Smoke/Test2.java
+++ src/main/java/Smoke/Test2.java
@@ -1,16 +1,22 @@
 package Smoke;
-
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Test2 {
 
-    @Test
-    public void test2 () {
-        System.out.println("Test 2");
+    // Провайдер позволяет использовать набор тестовых данных
+    @DataProvider (name = "provider")
+    public static Object[][] dbData () {
+        return new Object[][] {
+                {"SQL", new Integer(1)},
+                {"noSQL", new Integer(2)}
+        };
     }
 
-    @Test (groups = {"smokeTest"})
-    public void test22 () {
-        System.out.println("Test 22");
+    // Подключение провайдера и использование данных из него
+    @Test (dataProvider = "provider")
+    public void test2 (String elem1, int elem2) {
+        System.out.println("Test 2" + " " + elem1 + " " + elem2);
     }
+
 }
